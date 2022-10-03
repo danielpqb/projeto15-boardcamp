306d76f4cbd5b2ab62fb72f638a6ce2229fe88e7
danielpqb <danielpqb@gmail.com>
2022-10-03 13:40:30 -0300

    Categories router

---
+++ src/routers/categories.router.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+  getCategories,
+  postCategory,
+} from "../controllers/categories.controller.js";
+
+const router = express.Router();
+
+router.get("/categories", getCategories);
+router.post("/categories", postCategory);
+
+export default router;
